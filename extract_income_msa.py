7f82543654da120f3fd4ce2ba0be83a87e355404
Remi Louf <remilouf@gmail.com>
2015-04-05 14:01:40 +0100

    Script to extract the income of households per blockgroup for each msa

--- extract_income_msa.py
+++ extract_income_msa.py
@@ -10,14 +10,13 @@ import csv
 # Import data
 #
 
-## MSA to counties crosswalk
-county_to_msa = {}
-with open('data/crosswalks/msa_county.csv', 'r') as source:
+## MSA to blockgroup 
+bg_to_msa = {}
+with open('data/crosswalks/msa_blockgroup.csv', 'r') as source:
     reader = csv.reader(source, delimiter='\t')
     reader.next()
     for rows in reader:
-        county_to_msa[rows[1]] = rows[0]
-
+        bg_to_msa[rows[1]] = rows[0]
 
 ## Income data at block-group level
 incomes = {}
@@ -32,4 +31,31 @@ with open('data/income/us/household_incomes.csv', 'r') as source:
 #
 # Group by MSA
 #
+incomes_msa = {}
+in_msa = 0
+out_msa = 0
+for bg in incomes:
+    if bg in bg_to_msa:
+        in_msa += 1
+        msa = bg_to_msa[bg]
+        if msa not in incomes_msa:
+            incomes_msa[msa] = {}
+        incomes_msa[msa][bg] = incomes[bg]
+    else:
+        out_msa += 1
 
+print '%s blockgroups are inside MSAs'%in_msa
+print '%s blockgroups are outside MSAs'%out_msa
+
+
+
+#
+# Save the data
+#
+for msa in incomes_msa:
+    with open('data/income/msa/%s/income.csv'%msa, 'w') as output:
+        output.write("BLOCKGROUP FIP\tLess than $10000\t$10000-$14999\t$15000-$19999\t$20000-$24999\t$25000-$29999\t$30000-$34999\t$35000-$39999\t$40000-$44999\t$45000-$49999\t$50000-$59999\t$60000-$74999\t$75000-$99999\t$100000-$124999\t$125000-$149999\t$150000-$199999\t$200000 or more\n")
+        for bg in incomes_msa[msa]:
+            output.write(str(bg)+'\t')
+            output.write('\t'.join(map(str, incomes_msa[msa][bg])))
+            output.write('\n')
