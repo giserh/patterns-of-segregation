8a1cb68115ff101d5599b40a6af73b06eb97b204
Remi Louf <remilouf@gmail.com>
2015-04-05 13:15:20 +0100

    Add script to compute crosswalk between places and blockgroups

--- bin/crosswalk_place_blockgroup.py
+++ bin/crosswalk_place_blockgroup.py
@@ -1,4 +1,4 @@
-"""crosswalk_cousub_bg.py
+"""crosswalk_place_bg.py
 
 Get the crosswalk between county subdivisions and blockgroups. Could not be
 found online, so I am using geometrical methods.
@@ -10,18 +10,18 @@ from shapely.geometry import shape
 ## Only a few states are concerned
 con_states = ['09', '23', '25', '33', '50', '44']
 
-cousub_to_bg = {}
+place_to_bg = {}
 for state in con_states:
-    print "Extracting crosswalk between county subdivisions and block group for"
+    print "Extracting crosswalk between places and block group for"
     print "state %s"%state
     #
     # Import data
     #
     ## County subdivisions
-    cousub = {}
-    with fiona.open('data/shp/%s/countysub.shp'%state, 'r', 'ESRI Shapefile') as source:
+    place = {}
+    with fiona.open('data/shp/%s/places.shp'%state, 'r', 'ESRI Shapefile') as source:
         for f in source:
-            cousub[f['properties']['COUSUBFP00']] = shape(f['geometry'])
+            place[f['properties']['PLACEFP00']] = shape(f['geometry'])
 
     ## Block groups
     blockgroup = {}
@@ -32,25 +32,25 @@ for state in con_states:
 
     #
     # Compute intersections
-    #     Blockgroups are contained in county subdivisions and a simple
+    #     Blockgroups may be contained in places and a simple
     #     intersects() will return the surrounding blockgroups as well. We thus
     #     need to check that the intersection is not a line.
     #
-    for cs, cs_shape in cousub.iteritems():
-        cousub_to_bg[cs] = []
+    for cs, cs_shape in place.iteritems():
+        place_to_bg[cs] = []
         for bg, bg_shape in blockgroup.iteritems():
             if (bg_shape.intersects(cs_shape) and
                 bg_shape.intersection(cs_shape).geom_type not in ['LineString',
                                                                 'MultiLineString']):
-                cousub_to_bg[cs].append(bg)
+                place_to_bg[cs].append(bg)
 
 
 
 #
 # Save the results
 #
-with open('data/crosswalks/countysub_to_blockgroup.csv', 'w') as output:
-    output.write("COUNTY SUBDIVISION FIP\tBLOCKGROUP FIP\n")
-    for cs in cousub_to_bg:
-        for bg in cousub_to_bg[cs]:
+with open('data/crosswalks/place_blockgroup.csv', 'w') as output:
+    output.write("PLACE FIP\tBLOCKGROUP FIP\n")
+    for cs in place_to_bg:
+        for bg in place_to_bg[cs]:
             output.write("%s\t%s\n"%(cs, bg))
